7fa8a627e8719629e96182f40d026f9ce55195ad
Khai Fahmi <khaifahmi99@gmail.com>
2022-02-08 11:51:22 +0000

    chore: use ts-node to check for changes

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,10 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/', (_, res) => {
+  res.send(`This is a health check service API. Running fine at [${new Date().toDateString()}]`);
 });
 
 app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+  return console.log(`Health Check API is listening at http://localhost:${port}`);
 });

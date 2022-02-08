eac1a524753a1d18d11c4f922b1963912c134961
Khai Fahmi <khaifahmi99@gmail.com>
2022-02-08 12:14:52 +0000

    feat:  added paths with URL constant

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,40 @@
 import 'dotenv/config';
 import express from 'express';
+import { PATHS } from './constant/path';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (_, res) => {
-  res.send(`This is a health check service API. Running fine at [${new Date().toDateString()}]`);
+  return res.send(`This is a health check service API. Running fine at [${new Date().toDateString()}]`);
 });
+
+app.get('/all', (_, res) => {
+  const statuses = {};
+  Object.keys(PATHS).forEach((service) => statuses[service] = null);
+
+  res.send({
+    statuses
+  })
+});
+
+app.get('/service', (req, res) => {
+  if (!req.query.name) {
+    return res.status(200).send({
+      errorMessage: 'No Service Name Provided'
+    });
+  }
+
+  const pathKeys = Object.keys(PATHS);
+
+  if (!pathKeys.includes((req.query.name as string).toUpperCase())) {
+    return res.status(404).send({
+      errorMessage: 'Service Name Not Found'
+    });
+  }
+
+  res.send('OK');
+})
 
 app.listen(port, () => {
   return console.log(`Health Check API is listening at http://localhost:${port}`);
---
+++ src/constant/path.ts
@@ -0,0 +1,5 @@
+export const PATHS = {
+    NYAN: 'http://192.168.0.20',
+    WORDLE: 'http://192.168.0.20:5000',
+    SKY_OFFICE_CLIENT: 'http://192.168.0.20:3000',
+}
